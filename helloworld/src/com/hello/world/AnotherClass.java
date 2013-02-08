aef26c46f5ee482153129ff4319ad405c6c8f62c
Jorn Nordahl <jorntnordahl@gmail.com>
2013-02-07 17:33:42 -0800

    test

---
+++ helloworld/src/com/hello/world/AnotherClass.java
@@ -0,0 +1,27 @@
+/**
+ * AnotherClass.java
+ * Created: Feb 7, 2013
+ * (c) Copyright 2013 Cloud Cruiser Inc.
+ * All Rights Reserved
+ */
+package com.hello.world;
+
+import java.util.Date;
+
+/**
+ * @author Jorn
+ *
+ */
+public class AnotherClass
+{
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    new Date();
+
+  }
+
+}
