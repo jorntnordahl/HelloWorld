eabbc32370bc783983c60f1d356cb2d487c5c7bf
Jorn Nordahl <jorntnordahl@gmail.com>
2013-02-07 17:08:34 -0800

    commits

---
+++ helloworld/src/com/hello/world/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * HelloWorld.java
+ * Created: Feb 7, 2013
+ * (c) Copyright 2013 Cloud Cruiser Inc.
+ * All Rights Reserved
+ */
+package com.hello.world;
+
+import java.util.Date;
+
+/**
+ * @author Jorn
+ *
+ */
+public class HelloWorld
+{
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World: " + new Date().toLocaleString());
+  }
+
+}
---
+++ helloworld/src/com/hello/world/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * package-info.java
+ * Created: Feb 7, 2013
+ * (c) Copyright 2013 Cloud Cruiser Inc.
+ * All Rights Reserved
+ */
+/**
+ * @author Jorn
+ * 
+ */
+package com.hello.world;
